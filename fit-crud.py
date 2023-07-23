d0be15d7d2e4657d3b434812b05eaa6266a20050
genffy <genffyl@gmail.com>
2023-07-24 00:33:38 +0800

    feat：update fit sdk

--- fit-crud.py
+++ fit-crud.py
@@ -1,4 +1,4 @@
-import fitparse
+from garmin_fit_sdk import Decoder, Stream
 
 
 def show_ele_data(data):
@@ -8,22 +8,14 @@ def show_ele_data(data):
         print(" * {}: {}".format(data.name, data.value))
 
 
+def parser_file(file_path):
+    stream = Stream.from_file(f"./data/" + file_path)
+    decoder = Decoder(stream)
+    messages, errors = decoder.read()
+
+    print(errors)
+    print(messages)
+
+
 if __name__ == "__main__":
-    # Load the FIT file
-    fitfile = fitparse.FitFile("./data/<ACTIVITY_NAME>.fit")
-
-    # Iterate over all messages of type "record"
-    # (other types include "device_info", "file_creator", "event", etc)
-    for record in fitfile.get_messages("record"):
-        # last_data = record
-        # show_ele_data(last_data)
-        # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
-        for data in record:
-            show_ele_data(data)
-        #     # Print the name and value of the data (and the units if it has any)
-        #     if data.units:
-        #         print(" * {}: {} ({})".format(data.name, data.value, data.units))
-        #     else:
-        #         print(" * {}: {}".format(data.name, data.value))
-
-        print("---")
+    parser_file("258582237_ACTIVITY.fit")
