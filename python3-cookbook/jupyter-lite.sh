074f98fc0eb195d796e174a58d300a321d662050
Zhengfei Li <genffyl@gmail.com>
2024-02-20 16:09:52 +0800

    feat: add ipynb

---
+++ python3-cookbook/jupyter-lite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+yum install wget
+
+wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+./bin/micromamba shell init -s bash -p ~/micromamba
+source ~/.bashrc
+
+# activate the environment and install a new version of Python
+micromamba activate
+micromamba install python=3.10 -c conda-forge -y
+
+# install the dependencies
+python -m pip install -r requirements-jupyter-lite.txt
+
+# build the JupyterLite site
+jupyter lite --version
+jupyter lite build --contents python3-cookbook/ipynb --output-dir dist
+# jupyter lite build --output-dir dist
